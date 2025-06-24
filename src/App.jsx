e20f5c3f6f5f1c0ae2b65cefc27dee96aa9dd6bb
jeramyleon <jeramyleon@gmail.com>
2025-06-24 15:20:52 -0400

    gifs load on startup and search bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { use } from "react";
 import { createRoot } from "react-dom/client";
 // import axios from "axios"; // Maybe we'll need axios? 🤔
 import "./style.css";
+import { useState, useEffect } from "react";
+import axios from "axios"; 
+import SearchField from "./SearchField";
 
-// const GIPHY_API_KEY = "YOUR_API_KEY";
+const GIPHY_API_KEY = "t5xMiavNzfSlVB7lXV5ZNmxoa09vmPQG";
+const url = `http://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`;
 
 const App = () => {
+  const [trending, setTrending] = useState([]);
+  const fetchTrending = async () => {
+    try {
+      const response = await axios.get(url);
+      const responseData = response.data.data;
+      setTrending(responseData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
+  console.log("TRENDING", trending);
+
   return (
     <div className="app">
       <h1 className="title">Let's Make Some API Requests!</h1>
+      <ul>
+        {trending.map((gif) => {
+          return <li key={gif.id}><img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} /></li>;
+        })}
+      </ul>
+
+      <SearchField/>
     </div>
   );
 };
--- src/SearchField.jsx
+++ src/SearchField.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const SearchField = () => {
-  return <div></div>;
+  const [search, setSearch] = useState("lebron");
+  const [results, setResults] = useState([]);
+  const userSearch = search.split(" ").join("+");
+  const url = `http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=t5xMiavNzfSlVB7lXV5ZNmxoa09vmPQG`;
+  const fetchSearch = async () => {
+    try {
+      const response = await axios.get(url);
+      const data = response.data.data;
+      setResults(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchSearch();
+  }, []);
+
+  // console.log("Search", search);
+  console.log("RESULTS", results);
+
+  return (
+    <>
+      <input onChange={(e) => {setSearch(e.target.value)}} placeholder="search for gifs" type="text"></input><button>Search</button>
+      <ul>
+        {results.map((gif) => {
+          return <li key={gif.id}><img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} /></li>;
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default SearchField;
